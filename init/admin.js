f174c687d698e0a0795a1254ee7e15bae91e4852
张泽zze <zze.zhang@qunar.com>
2018-02-15 12:49:25 +0800

    init

---
+++ init/admin.js
@@ -0,0 +1,21 @@
+
+const mongoose    = require('mongoose')
+const User        = require('./../models/user')
+mongoose.connect('mongodb://127.0.0.1:27017/mymall', {useMongoClient: true})
+function initAdmin() {
+    return new Promise(resolve => {
+        User.insertMany({
+            "userId": "9527",
+            "name": "陈二狗",
+            "avatar": "http://osc9sqdxe.bkt.clouddn.com/defaultAvatar.jpg",
+            "userName": "admin",
+            "userPwd": "admin",
+            "orderList": [],
+            "cartList": [],
+            "addressList": []
+        }, () => {
+            resolve()
+        })
+    })
+}
+module.exports = initAdmin
