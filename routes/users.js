a1517f2a00aa177f9e82d73ac1d7a16099c37619
张泽zze <zze.zhang@qunar.com>
2018-05-05 16:30:16 +0800

    修改支付宝支付成功回调地址

--- routes/users.js
+++ routes/users.js
@@ -634,7 +634,7 @@ router.get('/aliPay', function (req, res) {
                                     // passbackParams: {},
                                     // extendParams: {},
                                     qrPayMode: 1,
-                                    return_url: `http://localhost:9999/#/order/paysuccess?price='${orderTotal}&orderId=${orderId}`
+                                    return_url: `http://39.107.236.248/#/order/paysuccess?price=${orderTotal}&orderId=${orderId}`
                                 });
         
                                 res.redirect('https://openapi.alipaydev.com/gateway.do?' + params)
