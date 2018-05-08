29eb5a36c5c699faa0beef5d99d65a64f80508f4
张泽zze <zze.zhang@qunar.com>
2018-05-09 00:00:40 +0800

    修改商品

--- routes/admin.js
+++ routes/admin.js
@@ -64,4 +64,68 @@ router.post('/changeRole', (req, res, next) => {
     });
 })
 
+// 查找商品
+router.post('/searchGood', (req, res, next) => {
+    let name = req.body.name || '',
+        id = req.body.id || '',
+        query
+        
+    if (name) {
+        query = {
+            productName: {
+                    $regex: new RegExp('\\.*' + name + '\\.*', 'i')
+                }
+            }
+    } else if (id) {
+        query = {
+            productId: {
+                $regex: new RegExp('\\.*' + id + '\\.*', 'i')
+            }
+        }
+    }
+    Good.find(query).then(goodArr => {
+        res.json({
+            status: 0,
+            message: 'suc',
+            result: goodArr
+        });
+    }).catch(err => {
+        res.json({
+            status: 1,
+            message: 'fail',
+            result: ''
+        })
+    });
+})
+
+// 查找商品
+router.post('/updateGood', (req, res, next) => {
+    let productId = req.body.productId,
+        productName = req.body.productName,
+        sub_title = req.body.sub_title,
+        salePrice = req.body.salePrice;
+        
+    Good.findOne({
+        productId
+    }).then(goodDoc => {
+        goodDoc.salePrice = salePrice;
+        goodDoc.productName = productName;
+        goodDoc.sub_title = sub_title;
+
+        return goodDoc.save();
+    }).then(() => {
+        res.json({
+            status: 0,
+            message: 'suc',
+            result: ''
+        });       
+    }).catch(err => {
+        res.json({
+            status: 1,
+            message: 'failed',
+            result: ''
+        })
+    });
+})
+
 module.exports = router
